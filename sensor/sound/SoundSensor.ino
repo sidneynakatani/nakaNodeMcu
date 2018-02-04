4c34901ced9330405e3cee9f4effaa0412af72f0
Sidney Hiroshi Nakatani <sidney.nakatani@hotmail.com>
2018-02-04 17:09:18 -0200

    Create SoundSensor.ino

---
+++ sensor/sound/SoundSensor.ino
@@ -0,0 +1,25 @@
+//This code use Arduino sketch Plotter Serial to read signal.
+// Tools > Plotter Serial
+
+//Pin A0 
+int analogic_pin = 0;
+
+
+// Initial Pin
+int value_A0 = 0;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(analogic_pin, INPUT);
+}
+
+void loop() {
+
+  value_A0 = analogRead(analogic_pin);
+
+  Serial.println(value_A0);
+  
+  delay(10);
+ 
+}
