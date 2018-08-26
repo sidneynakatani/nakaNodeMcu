885069816baff098b763eab22bd3ac1c5a0b119c
Sidney Hiroshi Nakatani <sidney.nakatani@hotmail.com>
2018-08-26 20:46:32 -0300

    Create tftDistanceSensor.ino

---
+++ display/tftDistanceSensor.ino
@@ -0,0 +1,58 @@
+#include <TFT_ILI9163C.h>    //utilizada para se comunicar com o módulo LCD
+const int isObstaclePin = 0; // Define o pino de entrada pin D3 (0)
+
+
+//Define as cores
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0 
+#define WHITE   0xFFFF
+ 
+#define __CS D1 // pino que conectaremos o CS do módulo LCD
+#define __DC D4 // pino que conectaremos o RS do módulo LCD
+
+TFT_ILI9163C display = TFT_ILI9163C(__CS, __DC); // construtor do objeto que utilizaremos para se comunicar com o módulo LCD
+
+String obstacle;
+int isObstacle = HIGH; 
+
+
+void setup(void) {
+   
+    Serial.begin(115200);
+    pinMode(isObstaclePin, INPUT);
+    display.begin(); // inicialização para se comunicar com o módulo LCD
+    display.clearScreen(); // limpa a tela, removendo todos os desenhos
+    display.fillScreen(BLACK); // pinta a tela toda de preto
+    display.setTextSize(2); // configura o tamanho do texto com o tamanho 2
+    display.setTextColor(GREEN); // configura a cor do texto como verde
+    display.setCursor(5,10); // posiciona o cursor para começar a escrita a partir do (x,y)
+  
+}
+
+void loop(){
+
+    delay(2000);
+    
+    isObstacle = digitalRead(isObstaclePin);
+
+    obstacle = "It is Ok!";
+
+    if (isObstacle == LOW){
+       display.setTextColor(RED); // configura a cor do texto como verde
+       obstacle = "OBSTACLE!";
+    }else{
+	display.setTextColor(GREEN); // configura a cor do texto como verde
+    }
+  
+    display.clearScreen(); // limpa a tela, removendo todos os desenhos
+    display.fillScreen(BLACK); // pinta a tela toda de preto
+    display.setTextSize(2); // configura o tamanho do texto com o tamanho 2
+    display.setCursor(5,10); // posiciona o cursor para começar a escrita a partir do (x,y)
+    display.print(""+ obstacle); // escreve em tela
+    
+}
